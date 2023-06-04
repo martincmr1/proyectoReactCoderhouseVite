aabdb3da93f9548e7026c855c6129005f85ae734
martincmr1 <martincmr1@hotmail.com>
2023-06-03 22:24:34 -0300

    coder36

--- my-vue-app/src/components/NavBar/NavBar.jsx
+++ my-vue-app/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,5 @@
 import './NavBar.css'
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
